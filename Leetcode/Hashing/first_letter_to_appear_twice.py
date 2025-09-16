ef78a5ff161cdba164f107b0c21a1705743d6245
yu-xiu <xiuyutiffany@gmail.com>
2025-09-16 16:00:19 -0700

    feat: add hashing first letter to appear twice

---
+++ Leetcode/Hashing/first_letter_to_appear_twice.py
@@ -0,0 +1,8 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        seen = set()
+        for c in s:
+            if c in seen:
+                return c
+            seen.add(c)
+        return ""
